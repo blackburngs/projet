bf78fe5c7fa0a9f4bb8ab5d2796bc5dc450c2fcd
nassim <nass_benab@hotmail.com>
2020-11-16 11:18:28 +0100

    done

---
+++ token.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const tokenSchema = new Schema({
+  token: "string",
+});
+
+const Token = mongoose.model("token", tokenSchema);
+
+module.exports = Token;
